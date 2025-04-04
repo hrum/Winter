a93f4bd5c2bf4d26cb5f642b56c7b3c939c3d9a1
Dmitry Shuvalov <dmit86@mail.ru>
2025-04-04 17:20:22 +0300

    changing field, player can now be caught by police

--- src/main/BuildingGenerator.java
+++ src/main/BuildingGenerator.java
@@ -1,79 +1,60 @@
 package main;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.util.*;
 
 public class BuildingGenerator {
 
-    private static final String PATH = "C:\\Users\\Zero\\Desktop\\Game projects\\winter\\cards.csv";
     private static final int LEVELS = 5;
-    private static final int LOWER_BOUND = -4;
-    private static final int UPPER_BOUND = 8;
-    private static final double BASE_PRICE = 2;
-    private static final double STEP = 0.1;
+    private static final int LOWER_BOUND = 1;
+    private static final int UPPER_BOUND = 12;
     private static final double ERROR_RATE = 0.55;
-    //three zones of risk: 15, 45, 75
 
     public static void main(String[] args) {
         BuildingGenerator generator = new BuildingGenerator();
-        generator.read();
+        /*for(int price = 2; price <= 12; price +=2) {
+            TreeMap<Double, List<Integer>> solutions = generator.generate(generator.getIncomeEV(price));
+            generator.select(solutions, 6);
+            System.out.println();
+        }*/
+        TreeMap<Double, List<Integer>> solutions = generator.generate(7);
+        generator.select(solutions, 18);
     }
 
-    private void read() {
-        try (BufferedReader reader = new BufferedReader(new FileReader(PATH))) {
-            String line = reader.readLine();
-            while(line != null) {
-                String[] lines = line.split(";");
-                int price = Integer.parseInt(lines[6]);
-                double compensation = Double.parseDouble(lines[4]);
-                double targetRisk = Double.parseDouble(lines[5]);
-                int[] solution = generate(getIncomeEV(price) + compensation, targetRisk);
-                String output = "";
-                for (int i = 0; i < LEVELS; i++) {
-                    output += solution[i] + ";";
-                }
-                System.out.println(output);
-                line = reader.readLine();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
-    }
-
-    private int[] generate(double targetEv, double targetRisk) {
-        double targetSum = targetEv * LEVELS;
-        int[] solution = new int[LEVELS];
-        double bestError = 1000;
+    private TreeMap<Double, List<Integer>> generate(double ev) {
+        double targetSum = ev * LEVELS;
+        TreeMap<Double, List<Integer>> solutions = new TreeMap<>();
+        double key = 0;
         for(int x1 = LOWER_BOUND; x1 <= UPPER_BOUND; x1++)
             for(int x2 = x1 + 1; x2 <= UPPER_BOUND; x2++)
                 for(int x3 = x2 + 1; x3 <= UPPER_BOUND; x3++)
                     for(int x4 = x3 + 1; x4 <= UPPER_BOUND; x4++)
                         for(int x5 = x4 + 1; x5 <= UPPER_BOUND; x5++) {
                             if(Math.abs(x1 + x2 + x3 + x4 + x5 - targetSum) < ERROR_RATE) {
-                                double curError = Math.abs(calcRisk(x1, x2, x3, x4, x5) - targetRisk);
-                                if(curError < bestError) {
-                                    solution[0] = x1;
-                                    solution[1] = x2;
-                                    solution[2] = x3;
-                                    solution[3] = x4;
-                                    solution[4] = x5;
-                                    bestError = curError;
-                                }
+                                key += 0.001;
+                                double risk = (x1 - ev)*(x1 - ev) + (x2 - ev)*(x2 - ev) + (x3 - ev)*(x3 - ev) + (x4 - ev)*(x4 - ev) + (x5 - ev)*(x5 - ev);
+                                List<Integer> solution = new ArrayList<>();
+                                solution.add(x1);
+                                solution.add(x2);
+                                solution.add(x3);
+                                solution.add(x4);
+                                solution.add(x5);
+                                //if(solutions.containsKey(risk))
+                                 //   System.out.println("overwriting solution");
+                                solutions.put(risk, solution);
                             }
                         }
-        return solution;
+        return solutions;
     }
 
-    private double calcRisk(int x1, int x2, int x3, int x4, int x5) {
-        double a = (double) (x1 + x2 + x3 + x4 + x5)/5;
-        return (x1 - a)*(x1 - a) + (x2 - a)*(x2 - a) + (x3 - a)*(x3 - a) + (x4 - a)*(x4 - a) + (x5 - a)*(x5 - a);
+    private void select(TreeMap<Double, List<Integer>> solutions, int number) {
+        for(int i = 0; i < number; i++) {
+            List<Integer> solution = solutions.pollFirstEntry().getValue();
+            System.out.println(solution);
+        }
     }
 
     private double getIncomeEV(int price) {
-        double ratio = price / BASE_PRICE;
-        return 0.7 * ratio * (1 - STEP * (ratio - 1)/2);
+        return 0.5 * price - 1;
     }
+
 }
